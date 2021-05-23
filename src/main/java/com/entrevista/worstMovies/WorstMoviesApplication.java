588d54d8afec584fb2d445500e19ce16a6ef9107
augustojtoso <81052179+augustojtoso@users.noreply.github.com>
2021-05-23 17:11:27 -0300

    Add files via upload
    pom

---
+++ src/main/java/com/entrevista/worstMovies/WorstMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.entrevista.worstMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorstMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorstMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entrevista/worstMovies/WorstMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entrevista.worstMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorstMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
